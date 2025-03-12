bd017b9103034d15e18c64d9cb8832e4a9c46619
shailu08 <shailendrasinghapogee@gmail.com>
2025-03-12 12:38:41 +0530

    initial commit

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const getallevents = `http://localhost:8080/events/getallevents`;
+const createevent = `http://localhost:8080/events/createevent`;
+const updateevent = `http://localhost:8080/events/updateevent`;
+const deleteevent = `http://localhost:8080/events/deleteevent`;
+const timezoneandlocation = `http://ip-api.com/json`;
+
+export { getallevents, createevent, updateevent, deleteevent, timezoneandlocation };
