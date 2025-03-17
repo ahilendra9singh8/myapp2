09577d08552c84c7f87dc938160392003d0c4e67
ahilendra9singh8 <shailendra9.singh8@gmail.com>
2025-03-17 17:48:30 +0530

    change in url

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
-// const getallevents = `http://localhost:8090/events/getallevents`;
-// const createevent = `http://localhost:8090/events/createevent`;
-// const updateevent = `http://localhost:8090/events/updateevent`;
-// const deleteevent = `http://localhost:8090/events/deleteevent`;
-// const addallowedip ="http://localhost:8090/events/addallowedip"
+// const getallevents = `http://localhost:8080/events/getallevents`;
+// const createevent = `http://localhost:8080/events/createevent`;
+// const updateevent = `http://localhost:8080/events/updateevent`;
+// const deleteevent = `http://localhost:8080/events/deleteevent`;
+// const addallowedip ="http://localhost:8080/events/addallowedip"
 // const timezoneandlocation = `http://ip-api.com/json`;
 
 // aws server
@@ -10,7 +10,7 @@ const getallevents = `http://13.201.83.130:8080/events/getallevents`;
 const createevent = `http://13.201.83.130:8080/events/createevent`;
 const updateevent = `http://13.201.83.130:8080/events/updateevent`;
 const deleteevent = `http://13.201.83.130:8080/events/deleteevent`;
-const addallowedip = `"http://13.201.83.130:8090/events/addallowedip"`;
+const addallowedip = `"http://13.201.83.130:8080/events/addallowedip"`;
 const timezoneandlocation = `http://ip-api.com/json`;
 
 export {
